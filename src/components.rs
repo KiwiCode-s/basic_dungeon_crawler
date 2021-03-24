edd155ab9e190c8336e0459e9596df74e94debfd
KiwiCode-s <38774014+KiwiCode-s@users.noreply.github.com>
2021-03-23 22:05:51 -0700

    Added HUD, tooltips, health system

--- src/components.rs
+++ src/components.rs
@@ -14,3 +14,22 @@ pub struct Enemy;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point,
+}
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Mana {
+    pub current: i32,
+    pub max: i32,
+}
+#[derive(Clone, Debug, PartialEq)]
+pub struct Name(pub String);
--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -1,20 +1,24 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();
-    movers.iter_mut(ecs).for_each(|(pos, _)| {
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    movers.iter_mut(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let destination = match rng.range(0, 4) {
-            0 => Point::new(-1, 0),
-            1 => Point::new(1, 0),
-            2 => Point::new(0, -1),
-            _ => Point::new(0, 1),
+            0 => Point::new(-1, 0), //left
+            1 => Point::new(1, 0),  //right
+            2 => Point::new(0, -1), //up
+            _ => Point::new(0, 1),  //down
         } + *pos;
-        if map.can_enter_tile(destination) {
-            *pos = destination;
-        }
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
     });
 }
