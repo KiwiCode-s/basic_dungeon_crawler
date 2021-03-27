9b22dcf2c03c4a5511472c4d58fb15426c100965
KiwiCode-s <38774014+KiwiCode-s@users.noreply.github.com>
2021-03-26 18:09:59 -0700

    added themeing and changes to maps

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,10 @@ mod prelude {
     pub use legion::systems::CommandBuffer;
     pub use legion::world::SubWorld;
     pub use legion::*;
-
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-
     pub use crate::camera::*;
     pub use crate::components::*;
     pub use crate::map::*;
@@ -47,14 +45,13 @@ impl State {
         spawn_player(&mut ecs, map_builder.player_start);
         spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
         map_builder
-            .rooms
+            .monster_spawns
             .iter()
-            .skip(1)
-            .map(|r| r.center())
-            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, *pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         resources.insert(TurnState::AwaitingInput);
+        resources.insert(map_builder.theme);
         Self {
             ecs,
             resources,
@@ -63,22 +60,38 @@ impl State {
             monster_systems: build_monster_scheduler(),
         }
     }
+
+    fn reset_game_state(&mut self) {
+        self.ecs = World::default();
+        self.resources = Resources::default();
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut self.ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        map_builder
+            .monster_spawns
+            .iter()
+            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, *pos));
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
+        self.resources.insert(map_builder.theme);
+    }
+
     fn game_over(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(2); // (1)
-        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended."); // (2)
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
         ctx.print_color_centered(
             4,
             WHITE,
             BLACK,
-            "Slain by a monster, your hero's journey has come to a \
-            premature end.",
+            "Slain by a monster, your hero's journey has come to a premature end.",
         );
         ctx.print_color_centered(
             5,
             WHITE,
             BLACK,
-            "The Amulet of YALA remains unclaimed, and your home town \
-            is not saved.",
+            "The Amulet of YALA remains unclaimed, and your home town is not saved.",
         );
         ctx.print_color_centered(
             8,
@@ -89,26 +102,8 @@ impl State {
         ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
 
         if let Some(VirtualKeyCode::Key1) = ctx.key {
-            // (3)
             self.reset_game_state();
         }
-    }
-    fn reset_game_state(&mut self) {
-        self.ecs = World::default();
-        self.resources = Resources::default();
-        let mut rng = RandomNumberGenerator::new();
-        let map_builder = MapBuilder::new(&mut rng);
-        spawn_player(&mut self.ecs, map_builder.player_start);
-        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
-        map_builder
-            .rooms
-            .iter()
-            .skip(1)
-            .map(|r| r.center())
-            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
-        self.resources.insert(map_builder.map);
-        self.resources.insert(Camera::new(map_builder.player_start));
-        self.resources.insert(TurnState::AwaitingInput);
     }
 
     fn victory(&mut self, ctx: &mut BTerm) {
@@ -118,8 +113,7 @@ impl State {
             4,
             WHITE,
             BLACK,
-            "You put on the Amulet of YALA and feel its power course through \
-            your veins.",
+            "You put on the Amulet of YALA and feel its power course through your veins.",
         );
         ctx.print_color_centered(
             5,
@@ -127,13 +121,7 @@ impl State {
             BLACK,
             "Your town is saved, and you can return to your normal life.",
         );
-        ctx.print_color_centered(
-            7,
-            GREEN,
-            BLACK,
-            "Press 1 to \
-            play again.",
-        );
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again.");
         if let Some(VirtualKeyCode::Key1) = ctx.key {
             self.reset_game_state();
         }
@@ -160,10 +148,9 @@ impl GameState for State {
                 self.player_systems
                     .execute(&mut self.ecs, &mut self.resources);
             }
-            TurnState::MonsterTurn => {
-                self.monster_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
             TurnState::GameOver => {
                 self.game_over(ctx);
             }
@@ -178,15 +165,15 @@ impl GameState for State {
 fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
-        .with_fps_cap(60.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (1)
-        .with_tile_dimensions(32, 32) // (2)
-        .with_resource_path("resources/") // (3)
-        .with_font("dungeonfont.png", 32, 32) // (4)
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
         .with_font("terminal8x8.png", 8, 8)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (5)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png") // (6)
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
---
+++ src/map_builder/themes.rs
@@ -0,0 +1,34 @@
+use crate::prelude::*;
+
+pub struct DungeonTheme {}
+
+impl DungeonTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self {})
+    }
+}
+
+impl MapTheme for DungeonTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Floor => to_cp437('.'),
+            TileType::Wall => to_cp437('#'),
+        }
+    }
+}
+pub struct FortestTheme {}
+
+impl MapTheme for FortestTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Floor => to_cp437(';'),
+            TileType::Wall => to_cp437('"'),
+        }
+    }
+}
+
+impl FortestTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self {})
+    }
+}
